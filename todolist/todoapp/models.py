77989f1e095a0253e12b92f49548f7ffadfd4530
tranthianhthu1808 <tranthianhthu18082002@gmail.com>
2024-01-24 16:08:19 +0700

    init project

--- todolist/todoapp/models.py
+++ todolist/todoapp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Todo(models.Model):
@@ -14,3 +17,15 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    finish_date = models.DateTimeField(null=True)
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['complete']
